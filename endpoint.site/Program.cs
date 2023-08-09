feda954d789f6fc882160cd8c3654e98c8016925
Arsham <arsham.fadaie@gmail.com>
2023-08-09 16:43:43 +0330

    first config Admin area

--- endpoint.site/Program.cs
+++ endpoint.site/Program.cs
@@ -24,7 +24,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapControllerRoute(
-      name: "areas",
-      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-    );
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+
 app.Run();
