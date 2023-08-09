2344ba860af81fcbfa463c0af5a48c62d5b23846
Arsham <arsham.fadaie@gmail.com>
2023-08-09 11:13:23 +0330

    Add project files.

---
+++ digitalShop.website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace digitalShop.website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
